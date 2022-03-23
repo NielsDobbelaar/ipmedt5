e7f31117d632fcaa8e19040e889985bf41b7c03b
Guidopi <guidoduif2002@gmail.com>
2022-03-23 12:23:56 +0100

    routes gemaakt voor versturen en ontvangen van berichten van esp

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/', function(Request $request) {
     \Illuminate\Support\Facades\Storage::append("log.txt", $request->getContent());
+});
+
+Route::get('/', function(){
+    return (new Response("banaan", 200))->header("Content-type", "text/plain");
 });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/sturen', function(){
+//     return response("banaan", 200)->header("Content-type", "text/plain");
+// });
