55e395252147cdeb2d92990b4a9999b41e611929
Guidopi <guidoduif2002@gmail.com>
2022-04-01 12:45:48 +0200

    timestamps toegevoegd aan database

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use \App\Models\Mailbox;
-use Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,8 +24,12 @@ Route::post('/', function(Request $request) {
     $data = $request->getContent();
     $lines = Storage::get('database.txt');
     $olddata = explode("\n", $lines);
-    if($data != end($olddata)){
-        Storage::disk("local")->append("database.txt", $data);
+    $time = now();
+
+    
+
+    if($data != explode(" ", end($olddata))[0]){
+        Storage::disk("local")->append("database.txt", $data . " " . $time);
     }
 });
 
@@ -39,5 +42,5 @@ Route::get('/', function(){
     // Storage::disk("local")->append("database2.txt", end($data));
 
 
-    return (new Response(end($data), 200))->header("Content-type", "text/plain");
+    return (new Response(explode(" ", end($data))[0], 200))->header("Content-type", "text/plain");
 });
